78e46ebdf4d0ac36326dbad971039607624ad9ca
fi5421 <24100032@lums.edu.pk>
2023-11-18 17:45:10 +0000

    committed single edge kitti run script

---
+++ Examples/Stereo/run_kitti.py
@@ -0,0 +1,136 @@
+import sys
+import os
+import subprocess
+import threading
+import signal
+import re
+import pandas as pd
+import time
+
+def server(port,event):
+    print("Server started")
+    args=["./stereo_kitti" ,'../../Vocabulary/ORBvoc.txt' ,'KITTI00-02.yaml' ,'server']
+    child_process=subprocess.Popen(args,stdin=subprocess.PIPE)
+    inp='127.0.0.1\n'+str(port)+'\n'+str(port+1)+'\n'+str(port+2)+'\n'
+    input=['127.0.0.1\n',str(port)+'\n',str(port+1)+'\n',str(port+2)+'\n']
+    count=0
+    print('writing',input[0])
+    child_process.stdin.write(bytes(input[0],'utf-8'))
+    print('writing',input[1])
+    child_process.stdin.write(bytes(input[1],'utf-8'))
+    print('writing',input[2])
+    child_process.stdin.write(bytes(input[2],'utf-8'))
+    print('writing',input[3])   
+    child_process.stdin.write(bytes(input[3],'utf-8'))
+    child_process.stdin.flush()
+    child_process.stdin.close()
+    print('here2')
+    print('event waiting')
+    event.wait()
+    print('signal sent1')
+    child_process.send_signal(signal.SIGINT)
+    child_process.wait()
+    print('signal sent2')
+
+def client(port,dataset,event):
+    print("client started")
+    args=["./stereo_kitti" ,'../../Vocabulary/ORBvoc.txt' ,'KITTI00-02.yaml' ,'client',dataset]
+    child_process=subprocess.Popen(args,stdin=subprocess.PIPE)
+    inp='127.0.0.1'+'\n'+'127.0.0.1'+'\n'+str(port)+'\n'+str(port)+'\n'+str(port+1)+'\n'+str(port+1)+'\n'+str(port+2)+'\n'+str(port+2)+'\n'
+    child_process.communicate(input=bytes(inp,'utf-8'))
+    child_process.wait()
+    print('client finished')
+    return
+
+def run_evo(gt,traj='KeyFrameTrajectory_TUM_Format1.txt'):
+    evo_proc=subprocess.Popen(['evo_ape','tum',gt,traj,'-va'],stdout=subprocess.PIPE)
+    out,err=evo_proc.communicate()
+    print(err)
+    out=out.decode('utf-8').split('\n')
+    # print('b4 proc',out)
+    if len(out)<10:
+        out=['error in evo' for i in range(7)]
+        return out
+    out=[i for i in out if i!='']
+    for i in range(len(out)):
+        
+        # string=string.replace('\t',' ')
+        string = re.sub('[^0-9.\t]', '', out[i])
+        string=string.replace('\t',"")
+        try:
+            out[i]=float(string)
+        except:
+            out[i]=0
+    # print(out[-7:])
+    return out[-7:]
+
+if len(sys.argv) != 5:
+    print("Usage: python run_kitti.py <dataset> <portStart> <gt> <run_times>")
+    sys.exit(1)
+
+dataset = sys.argv[1]
+portStart = int(sys.argv[2])
+index=['max','mean','median','min','rmse','sse','std','numKFS']
+df=pd.DataFrame(columns=index)
+print(df)
+
+runs=int(sys.argv[4])
+
+for i in range(runs):
+    
+
+    event=threading.Event()
+    server_thread = threading.Thread(target=server, args=(portStart,event,))
+    server_thread.start()
+
+    time.sleep(1)
+
+    client_thread = threading.Thread(target=client, args=(portStart,dataset,event,))
+    client_thread.start()
+
+    # print('waiting for threads to finish')
+    client_thread.join()
+    # print('client joined')
+    event.set()
+    server_thread.join()
+    print('server joined')
+    gt=sys.argv[3]
+
+    evo_res=run_evo(gt)
+    print('res',evo_res)
+
+    file=open('KeyFrameTrajectory_TUM_Format1.txt','r')
+    lines=file.readlines()
+    num=len(lines)
+
+    print(evo_res+[num])
+    df.loc[-1]=evo_res+[num]
+    df.index = df.index + 1
+
+    portStart+=3
+
+    print(df.head())
+
+
+
+
+# evo_proc=subprocess.Popen(['evo_ape','tum',gt,'KeyFrameTrajectory_TUM_Format1.txt','-va'],stdout=subprocess.PIPE)
+# out,err=evo_proc.communicate()
+# print(err)
+# out=out.decode('utf-8').split('\n')
+# out=[i for i in out if i!='']
+# for i in range(len(out)):
+    
+#     # string=string.replace('\t',' ')
+#     string = re.sub('[^0-9.\t]', '', out[i])
+#     string=string.replace('\t',"")
+#     try:
+#         out[i]=float(string)
+#     except:
+#         out[i]=0
+# print(out[-7:])
+
+
+# evo_proc.wait()
+
+
